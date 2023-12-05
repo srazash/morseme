97c55f960583928ea0044e3a5cc71c83dbdd9e25
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-12-05 17:47:30 +0000

    All tests now passing

--- server/morsecode/morsecode_test.go
+++ server/morsecode/morsecode_test.go
@@ -33,9 +33,9 @@ func TestEncodeWithSpaces(t *testing.T) {
 
 func TestEncodeWithNonAlphaChars(t *testing.T) {
 	input := "Hej!"
-	want := ".... . .---"
-	output, _ := Encode(input)
-	if output != want {
-		t.Fatalf("Output: %s does not match want: %s\n", output, want)
+	want := ""
+	output, err := Encode(input)
+	if output != want && err != nil {
+		t.Fatalf("Output: %s does not match want: %s, and error should not be nil\n", output, want)
 	}
 }
