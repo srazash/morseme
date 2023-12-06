f1431411dc4360a626d9fd34aa6e6cb6f9143a77
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-12-06 19:01:03 +0000

    Form submit is IN PROGRESS and needs finishing!

--- server/server.go
+++ server/server.go
@@ -28,5 +28,9 @@ func main() {
 		return c.HTML(http.StatusOK, `<h1 id="h1-title" hx-get="/title-morse" hx-trigger="click" hx-target="#h1-title" hx-swap="outerHTML">MorseMe</h1>`)
 	})
 
+	e.POST("/encode-to-morse", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `<pre id="encode-output" class="big">Submitted!</pre>`)
+	})
+
 	e.Logger.Fatal(e.Start(":3000"))
 }
