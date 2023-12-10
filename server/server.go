cbe2f311107a0731308f2ed729fafa7239a29335
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-12-10 19:44:54 +0000

    Encode form working

--- server/server.go
+++ server/server.go
@@ -31,13 +31,29 @@ func main() {
 	})
 
 	e.POST("/encode-to-morse", func(c echo.Context) error {
-		m := ""
+		pre := ""
 		enc, err := morsecode.Encode(c.FormValue("text-input"))
 		if err != nil {
-			m = `<pre id="encode-output" class="big">Please only use letters and spaces!</pre>`
+			pre = `<pre id="encode-output" class="big">Please only use letters and spaces!</pre>`
 		} else {
-			m = fmt.Sprintf(`<pre id="encode-output" class="big">%s</pre>`, enc)
+			pre = fmt.Sprintf(`<pre id="encode-output" class="big">%s</pre>`, enc)
 		}
+
+		m := fmt.Sprintf(`<figure id="encode-figure">
+		<h2>Text to Morse Test</h2>
+		<form id="encode-form" class="box rows"
+			hx-target="#encode-figure"
+			hx-swap="outerHTML"
+			hx-post="/encode-to-morse">
+				<label for="text-input">
+					Input:
+					<input id="text-input" name="text-input" type="text" size="40" spellcheck="true" placeholder="letters and spaces only" maxlength="100" required />
+				</label>
+			<input class="big" type="submit" value="Encode!" />
+		</form>
+		%s
+	</figure>`, pre)
+
 		return c.HTML(http.StatusOK, m)
 	})
 
