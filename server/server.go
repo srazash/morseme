8110195b30ee2704df9c9f24d263b0e66cd76017
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-12-19 15:49:14 +0000

    All markup in server.go moved to templ templates

--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"morseme/server/morsecode"
 	"morseme/server/templates"
 	"net/http"
@@ -41,36 +40,10 @@ func main() {
 	})
 
 	e.POST("/encode-to-morse", func(c echo.Context) error {
-		d := ""
+		m := new(bytes.Buffer)
 		enc, err := morsecode.Encode(c.FormValue("text-input"))
-		if err != nil {
-			d = `<div id="encode-output" class="terminal-alert terminal-alert-error">invalid input: letters and spaces only!</div>`
-		} else {
-			d = fmt.Sprintf(`<div id="encode-output" class="terminal-alert terminal-alert-primary">%s</div>`, enc)
-		}
-
-		m := fmt.Sprintf(`<form id="encode-form"
-		hx-target="#encode-form"
-		hx-swap="outerHTML"
-		hx-post="/encode-to-morse">
-		<fieldset>
-			<legend>Text to convert</legend>
-			<div>
-			<label for="text-input">
-				Input:
-				<input id="text-input" name="text-input" type="text" size="40" spellcheck="true" placeholder="letters and spaces only" maxlength="100" required />
-			</label>
-			</div>
-			<br>
-			<div>
-				<input id="submit-btn" class="btn btn-default" type="submit" value="Convert" />
-			</div>
-		</fieldset>
-		<br>
-		%s
-		</form>`, d)
-
-		return c.HTML(http.StatusOK, m)
+		templates.Encode(enc, err).Render(context.Background(), m)
+		return c.HTML(http.StatusOK, m.String())
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
